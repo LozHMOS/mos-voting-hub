64eae06fb90acc51c7a9cef4cc2b7d3d1d909e61
LozHMOS <lozh@mappedoutsolutions.com>
2026-04-20 23:06:23 +1000

    Update 4_idea_details.py

--- pages/4_idea_details.py
+++ pages/4_idea_details.py
@@ -1,16 +1,21 @@
 import streamlit as st
+import pandas as pd
 from utils.db import fetch_ideas, fetch_votes
 
 st.title("🔍 Idea Details & Comments")
 
 ideas = fetch_ideas()
+if not ideas:
+    st.info("No ideas submitted yet.")
+    st.stop()
+
 idea_options = {idea["name"]: idea for idea in ideas}
 selected_name = st.selectbox("Select an idea", options=list(idea_options.keys()))
 idea = idea_options[selected_name]
 
 st.subheader(idea["name"])
 st.write(idea["description"])
-if idea["demo_link"]:
+if idea.get("demo_link"):
     st.markdown(f"[View Demo]({idea['demo_link']})")
 
 st.markdown("### Full Readme / Information")
@@ -20,6 +25,8 @@ votes = fetch_votes(idea["id"])
 if votes:
     st.subheader("Scores from team")
     df = pd.DataFrame(votes)
-    st.dataframe(df[["voter_name", "cost", "complexity", "resourcing", "revenue_potential", "strategic_fit", "comment"]], use_container_width=True)
+    # Show only the columns we care about
+    display_cols = ["voter_name", "cost", "complexity", "resourcing", "revenue_potential", "strategic_fit", "comment"]
+    st.dataframe(df[display_cols], use_container_width=True)
 else:
-    st.info("No votes yet.")
+    st.info("No votes yet for this idea.")
